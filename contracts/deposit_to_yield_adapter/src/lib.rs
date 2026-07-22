cbd8258e1f731ff7bf622bc0a343a1654e9870e5
strangedev <strngecloud.iv@gmail.com>
2026-07-23 00:21:01 +0100

    feat: add safety lint attributes to deposit_to_yield_adapter
    - Deny unsafe code
    - Enforce clippy checks
    - Enable pedantic lints
    
    Closes #258

--- contracts/deposit_to_yield_adapter/src/lib.rs
+++ contracts/deposit_to_yield_adapter/src/lib.rs
@@ -1,4 +1,14 @@
 #![no_std]
+// Safety lint attributes for smart contract
+#![deny(unsafe_code)]
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    unused_results,
+    clippy::all,
+    clippy::pedantic
+)]
+
 use soroban_sdk::{
     contract,
     contractimpl,
